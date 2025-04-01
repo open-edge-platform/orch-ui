2543e8ac2321a733423ecedeb0b3c7e785fab90a
Rahul Gautham Putcha <rahul.gautham.putcha@intel.com>
2025-04-01 14:45:45 -0400

    [ITEP-24904] fix manual deployment cluster drawer

--- apps/app-orch/src/components/organisms/setup-deployments/SelectCluster/SelectCluster.cy.tsx
+++ apps/app-orch/src/components/organisms/setup-deployments/SelectCluster/SelectCluster.cy.tsx
@@ -3,22 +3,57 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import SelectCluster from "./SelectCluster";
+import { Button } from "@spark-design/react";
+import React from "react";
+import SelectCluster, { SelectClusterMode } from "./SelectCluster";
 import SelectClusterPom from "./SelectCluster.pom";
+
+const ClusterListMock = ({
+  onShowDetails,
+}: {
+  onShowDetails: (clusterId: string) => void;
+}) => {
+  return (
+    <Button
+      data-cy="testShowDrawer"
+      onPress={() => {
+        onShowDetails("cluster1");
+      }}
+    >
+      cluster1
+    </Button>
+  );
+};
 
 const pom = new SelectClusterPom();
 describe("<SelectCluster/>", () => {
   beforeEach(() => {
     cy.mount(
       <SelectCluster
-        onDeploymentNameChange={() => {}}
         selectedIds={[]}
-        onSelect={() => {}}
+        onSelect={cy.stub().as("onSelectStub")}
+        onDeploymentNameChange={cy.stub().as("onDeploymentNameChangeStub")}
+        mode={SelectClusterMode.CREATE}
+        __ClusterListRemote={React.lazy(() =>
+          Promise.resolve({
+            default: () => (
+              <ClusterListMock onShowDetails={cy.stub().as("onShowDetails")} />
+            ),
+          }),
+        )}
       />,
     );
   });
   it("should render component", () => {
     pom.root.should("exist");
     pom.el.title.contains("Enter Deployment Details");
+  });
+
+  it("should show cluster details", () => {
+    // This will avoid the error shown when no name is entered
+    pom.el.deploymentNameField.type("test");
+
+    cy.get("[data-cy='testShowDrawer']").click();
+    cy.get("@onShowDetails").should("be.calledWith", "cluster1");
   });
 });
--- apps/app-orch/src/components/organisms/setup-deployments/SelectCluster/SelectCluster.pom.ts
+++ apps/app-orch/src/components/organisms/setup-deployments/SelectCluster/SelectCluster.pom.ts
@@ -5,8 +5,6 @@
 
 import { SiTablePom } from "@orch-ui/poms";
 import { Cy, CyPom } from "@orch-ui/tests";
-import { dataCy as clusterDetailDataCy } from "../ClusterDetails/ClusterDetails";
-import ClusterDetailsPom from "../ClusterDetails/ClusterDetails.pom";
 
 const dataCySelectors = ["title", "deploymentNameField"] as const;
 type Selectors = (typeof dataCySelectors)[number];
@@ -14,11 +12,9 @@ type ApiAliases = "getClusterSuccess";
 
 class SelectClusterPom extends CyPom<Selectors, ApiAliases> {
   public clusterTable: SiTablePom;
-  public clusterDetail: ClusterDetailsPom;
   constructor(public rootCy: string = "selectCluster") {
     super(rootCy, [...dataCySelectors]);
     this.clusterTable = new SiTablePom("table");
-    this.clusterDetail = new ClusterDetailsPom(clusterDetailDataCy);
   }
 
   public selectNthCluster(n: number): Cy {
