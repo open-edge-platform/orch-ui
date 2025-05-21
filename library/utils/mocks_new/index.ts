233473a1613fdd40d81b9104b8452a179bfde74b
Chandrasekharan, Vignesh <vignesh.chandrasekharan@intel.com>
2025-05-21 19:54:26 +0530

    remove infra code from this branch

--- library/utils/mocks_new/index.ts
+++ library/utils/mocks_new/index.ts
@@ -3,11 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// import { hosts } from "../mocks/infra";
 import { handlers as alerts } from "./admin/handlers/alerts";
-// import { handlers as hosts } from "./infra/handlers/hosts";
 
-// const handlersInfra = [...regions, ...hosts];
 const handlersAdmin = [...alerts];
 
 // mocks expose one collection of all handlers
