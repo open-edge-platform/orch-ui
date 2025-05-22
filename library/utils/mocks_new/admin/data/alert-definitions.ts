edb559cc64fc820dbc412eae06400bf0c4107bcc
Chandrasekharan, Vignesh <vignesh.chandrasekharan@intel.com>
2025-05-22 16:00:18 +0530

    semantic change

--- library/utils/mocks_new/admin/data/alert-definitions.ts
+++ library/utils/mocks_new/admin/data/alert-definitions.ts
@@ -5,69 +5,69 @@
 
 import { generateAlertDefinition } from "../data.helpers";
 import {
-  alertDefinitionEightId,
-  alertDefinitionFiveId,
-  alertDefinitionFourId,
-  alertDefinitionNineId,
-  alertDefinitionOneId,
-  alertDefinitionSevenId,
-  alertDefinitionSixId,
-  alertDefinitionTenId,
-  alertDefinitionThreeId,
-  alertDefinitionTwoId,
+  ALERT_DEFINITION_EIGHT_ID,
+  ALERT_DEFINITION_FIVE_ID,
+  ALERT_DEFINITION_FOUR_ID,
+  ALERT_DEFINITION_NINE_ID,
+  ALERT_DEFINITION_ONE_ID,
+  ALERT_DEFINITION_SEVEN_ID,
+  ALERT_DEFINITION_SIX_ID,
+  ALERT_DEFINITION_TEN_ID,
+  ALERT_DEFINITION_THREE_ID,
+  ALERT_DEFINITION_TWO_ID,
 } from "./ids/alert-definitions";
 
 // Generate alert definitions using the helper function
 export const alertDefinitionOne = generateAlertDefinition(
-  alertDefinitionOneId,
+  ALERT_DEFINITION_ONE_ID,
   "HostConnectionLost",
 );
 
 export const alertDefinitionTwo = generateAlertDefinition(
-  alertDefinitionTwoId,
+  ALERT_DEFINITION_TWO_ID,
   "HostError",
 );
 
 export const alertDefinitionThree = generateAlertDefinition(
-  alertDefinitionThreeId,
+  ALERT_DEFINITION_THREE_ID,
   "HostCPUUsage",
 );
 
 export const alertDefinitionFour = generateAlertDefinition(
-  alertDefinitionFourId,
+  ALERT_DEFINITION_FOUR_ID,
   "HostRAMUsage",
   "new",
 );
 
 export const alertDefinitionFive = generateAlertDefinition(
-  alertDefinitionFiveId,
+  ALERT_DEFINITION_FIVE_ID,
   "DeploymentDown",
 );
 
 export const alertDefinitionSix = generateAlertDefinition(
-  alertDefinitionSixId,
+  ALERT_DEFINITION_SIX_ID,
   "DeploymentError",
 );
 
 export const alertDefinitionSeven = generateAlertDefinition(
-  alertDefinitionSevenId,
+  ALERT_DEFINITION_SEVEN_ID,
   "ClusterDown",
   "new",
 );
 
 export const alertDefinitionEight = generateAlertDefinition(
-  alertDefinitionEightId,
+  ALERT_DEFINITION_EIGHT_ID,
   "ClusterError",
 );
 
 export const alertDefinitionNine = generateAlertDefinition(
-  alertDefinitionNineId,
+  ALERT_DEFINITION_NINE_ID,
   "ClusterCPUUsage",
   "applied",
   { threshold: "30", duration: "5m" },
 );
 
 export const alertDefinitionTen = generateAlertDefinition(
-  alertDefinitionTenId,
+  ALERT_DEFINITION_TEN_ID,
   "ClusterRAMUsage",
 );
--- library/utils/mocks_new/admin/data/ids/alert-definitions.ts
+++ library/utils/mocks_new/admin/data/ids/alert-definitions.ts
@@ -3,13 +3,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export const alertDefinitionOneId = "Host-ConnectionLostID";
-export const alertDefinitionTwoId = "Host-Error-ID";
-export const alertDefinitionThreeId = "Host-CPUUsageID";
-export const alertDefinitionFourId = "Host-RAM-Usage-ID";
-export const alertDefinitionFiveId = "Deployment-Down-ID";
-export const alertDefinitionSixId = "Deployment-Error-ID";
-export const alertDefinitionSevenId = "Cluster-Down-ID";
-export const alertDefinitionEightId = "Cluster-Error-ID";
-export const alertDefinitionNineId = "Cluster-CPU-Usage-ID";
-export const alertDefinitionTenId = "Cluster-RAM-Usage-ID";
+export const ALERT_DEFINITION_ONE_ID = "Host-ConnectionLostID";
+export const ALERT_DEFINITION_TWO_ID = "Host-Error-ID";
+export const ALERT_DEFINITION_THREE_ID = "Host-CPUUsageID";
+export const ALERT_DEFINITION_FOUR_ID = "Host-RAM-Usage-ID";
+export const ALERT_DEFINITION_FIVE_ID = "Deployment-Down-ID";
+export const ALERT_DEFINITION_SIX_ID = "Deployment-Error-ID";
+export const ALERT_DEFINITION_SEVEN_ID = "Cluster-Down-ID";
+export const ALERT_DEFINITION_EIGHT_ID = "Cluster-Error-ID";
+export const ALERT_DEFINITION_NINE_ID = "Cluster-CPU-Usage-ID";
+export const ALERT_DEFINITION_TEN_ID = "Cluster-RAM-Usage-ID";
