62fd007fccf26288a2b691993b7c2d0507886235
Chandrasekharan, Vignesh <vignesh.chandrasekharan@intel.com>
2025-05-26 14:14:21 +0530

    change baseUrl

--- library/utils/mocks_new/admin/handlers/alerts.ts
+++ library/utils/mocks_new/admin/handlers/alerts.ts
@@ -3,19 +3,14 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { SharedStorage } from "@orch-ui/utils";
 import { rest } from "msw";
+import { baseURL } from "../base-url";
 import AlertStore from "../store/alerts";
-
-const baseURL = "/v1";
 
 const as = new AlertStore();
 
-const projectName = SharedStorage.project?.name;
-
-// alerts
 export const handlers = [
-  rest.get(`${baseURL}/projects/${projectName}/alerts`, (_, res, ctx) => {
+  rest.get(`${baseURL}/alerts`, (_, res, ctx) => {
     return res(ctx.status(200), ctx.json({ alerts: as.list() }));
   }),
 ];
