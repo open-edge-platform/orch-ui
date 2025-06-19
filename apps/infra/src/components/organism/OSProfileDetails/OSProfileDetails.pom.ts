0ff62f8c06091ca12559cacec0fb822c6b3e9918
riyaz-ghati <108117001+riyaz-ghati@users.noreply.github.com>
2025-06-19 19:50:47 +0530

    CVE details in OS profile (#188)
    Co-authored-by: satya-in <satyaranjan.acharya@intel.com>

--- apps/infra/src/components/organism/OSProfileDetails/OSProfileDetails.pom.ts
+++ apps/infra/src/components/organism/OSProfileDetails/OSProfileDetails.pom.ts
@@ -3,13 +3,20 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { TablePom } from "@orch-ui/components";
 import { CyPom } from "@orch-ui/tests";
 
-const dataCySelectors = [] as const;
+const dataCySelectors = ["installedPackagesRoot", "cveTabRoot"] as const;
 type Selectors = (typeof dataCySelectors)[number];
 
 export class OSProfileDetailsPom extends CyPom<Selectors> {
+  public table: TablePom;
   constructor(public rootCy: string = "osProfileDetails") {
     super(rootCy, [...dataCySelectors]);
+    this.table = new TablePom();
+  }
+
+  getTab(tabName: string) {
+    return this.root.find(".spark-tabs-tab").contains(tabName);
   }
 }
