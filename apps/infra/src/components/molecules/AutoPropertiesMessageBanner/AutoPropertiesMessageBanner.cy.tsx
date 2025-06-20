bc8a053cb9771163d0332fa897f0dd15567a15e5
Chandrasekharan, Vignesh <vignesh.chandrasekharan@intel.com>
2025-06-20 14:15:17 +0530

    message test updates

--- apps/infra/src/components/molecules/AutoPropertiesMessageBanner/AutoPropertiesMessageBanner.cy.tsx
+++ apps/infra/src/components/molecules/AutoPropertiesMessageBanner/AutoPropertiesMessageBanner.cy.tsx
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { initialState } from "../../../store/configureHost";
+// import { initialState } from "../../../store/configureHost";
+import { initialState } from "apps/infra/src/store/provisionHost";
 import { setupStore } from "../../../store/store";
 import AutoPropertiesMessageBanner from "./AutoPropertiesMessageBanner";
 import AutoPropertiesMessageBannerPom from "./AutoPropertiesMessageBanner.pom";
@@ -18,11 +19,11 @@ describe("<AutoPropertiesMessageBanner/>", () => {
 
   it("Should render correct default message", () => {
     const store = setupStore({
-      configureHost: {
+      provisionHost: {
+        ...initialState,
         autoOnboard: false,
         autoProvision: false,
-        formStatus: { ...initialState.formStatus },
-        hosts: {},
+        createCluster: false,
       },
     });
     cy.mount(<AutoPropertiesMessageBanner />, {
@@ -33,11 +34,11 @@ describe("<AutoPropertiesMessageBanner/>", () => {
 
   it("Should render correct onboard only message", () => {
     const store = setupStore({
-      configureHost: {
+      provisionHost: {
+        ...initialState,
         autoOnboard: true,
         autoProvision: false,
-        formStatus: { ...initialState.formStatus },
-        hosts: {},
+        createCluster: false,
       },
     });
     cy.mount(<AutoPropertiesMessageBanner />, {
@@ -48,11 +49,11 @@ describe("<AutoPropertiesMessageBanner/>", () => {
 
   it("Should render correct provison only message", () => {
     const store = setupStore({
-      configureHost: {
+      provisionHost: {
+        ...initialState,
         autoOnboard: false,
         autoProvision: true,
-        formStatus: { ...initialState.formStatus },
-        hosts: {},
+        createCluster: false,
       },
     });
     cy.mount(<AutoPropertiesMessageBanner />, {
@@ -63,16 +64,31 @@ describe("<AutoPropertiesMessageBanner/>", () => {
 
   it("Should render correct onboard & provision message", () => {
     const store = setupStore({
-      configureHost: {
+      provisionHost: {
+        ...initialState,
         autoOnboard: true,
         autoProvision: true,
-        formStatus: { ...initialState.formStatus },
-        hosts: {},
+        createCluster: false,
       },
     });
     cy.mount(<AutoPropertiesMessageBanner />, {
       reduxStore: store,
     });
     pom.root.invoke("text").should("eq", AutoPropertiesMessages.BothSelected);
+  });
+
+  it("Should render correct message when onboard, provision, and cluster creation are all enabled", () => {
+    const store = setupStore({
+      provisionHost: {
+        ...initialState,
+        autoOnboard: true,
+        autoProvision: true,
+        createCluster: true,
+      },
+    });
+    cy.mount(<AutoPropertiesMessageBanner />, {
+      reduxStore: store,
+    });
+    pom.root.invoke("text").should("eq", AutoPropertiesMessages.CreateCluster);
   });
 });
