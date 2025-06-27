dbbd36d6f8dc0c1e32bcafa22e59cbec55b5fd23
riyaz C Ghati <riyaz.chandasaheb.ghati@intel.com>
2025-06-27 13:16:15 +0530

    Integrated api and added validation

--- apps/app-orch/src/components/organisms/deploymentPackages/DeploymentPackageHelmChartInfoForm/DeploymentPackageHelmChartInfoForm.tsx
+++ apps/app-orch/src/components/organisms/deploymentPackages/DeploymentPackageHelmChartInfoForm/DeploymentPackageHelmChartInfoForm.tsx
@@ -3,26 +3,18 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Flex, Textarea } from "@orch-ui/components";
-import {
-  Button,
-  FieldLabel,
-  TextField,
-  ToggleSwitch,
-} from "@spark-design/react";
-import {
-  ButtonSize,
-  ButtonVariant,
-  FieldLabelSize,
-  InputSize,
-} from "@spark-design/tokens";
-import { Control, Controller, FieldErrors } from "react-hook-form";
+import { catalog } from "@orch-ui/apis";
+import { AdvancedSettingsToggle, Flex } from "@orch-ui/components";
+import { SharedStorage } from "@orch-ui/utils";
+import { Button, FieldLabel, Heading, TextField } from "@spark-design/react";
+import { ButtonSize, ButtonVariant, InputSize } from "@spark-design/tokens";
+import { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../../../store/hooks";
 import {
   clearDeploymentPackage,
   selectDeploymentPackage,
-  setDescription,
   setHelmChartURL,
   setPassword,
   setUsername,
@@ -35,35 +27,93 @@ export type PackageInputs = {
   helmChartURL: string;
   username: string;
   password: string;
-  description: string;
 };
 
-interface DeploymentPackageGeneralInfoFormProps {
-  control: Control<PackageInputs, string>;
-  errors: FieldErrors<PackageInputs>;
-  includeAuth: boolean;
-  handleImport: () => void;
-  toggleAuthSwitch: (boolean) => void;
-}
-
-const DeploymentPackageHelmChartInfoForm = ({
-  control,
-  errors,
-  handleImport,
-  toggleAuthSwitch,
-  includeAuth = false,
-}: DeploymentPackageGeneralInfoFormProps) => {
+const DeploymentPackageHelmChartInfoForm = () => {
   const cy = { "data-cy": dataCy };
-  const className = "deployment-package-import-helm-chart-info-form";
-  console.log("control:", control);
+  const className = "dp-helmchart-form";
+  const [advancedSettings, setAdvancedSettings] = useState<boolean>(false);
+  // Add local state for validation errors
+  const [validationErrors, setValidationErrors] = useState({
+    username: "",
+    password: "",
+  });
+
   const dispatch = useAppDispatch();
-  const { description } = useAppSelector(selectDeploymentPackage);
   const navigate = useNavigate();
+  const [importDeploymentPackage] = catalog.useCatalogServiceImportMutation();
+  const deploymentPackage = useAppSelector(selectDeploymentPackage);
+  const {
+    control,
+    formState: { errors },
+    clearErrors,
+    setValue,
+  } = useForm<PackageInputs>({
+    mode: "all",
+    defaultValues: {
+      helmChartURL: deploymentPackage.helmChartURL,
+      username: deploymentPackage.username,
+      password: deploymentPackage.password,
+    },
+  });
+
+  // Watch the field values for validation
+  const username = deploymentPackage.username;
+  const password = deploymentPackage.password;
+
+  useEffect(() => {
+    // Clear previous errors
+    setValidationErrors({ username: "", password: "" });
+
+    const hasUsername = !!username?.trim();
+    const hasPassword = !!password?.trim();
+
+    // Set errors based on field values
+    if (hasUsername && !hasPassword) {
+      setValidationErrors((prev) => ({
+        ...prev,
+        password: "Password is required when username is provided",
+      }));
+    } else if (!hasUsername && hasPassword) {
+      setValidationErrors((prev) => ({
+        ...prev,
+        username: "Username is required when password is provided",
+      }));
+    }
+  }, [username, password]);
+
+  // Clear validation errors when advanced settings are toggled off
+  useEffect(() => {
+    if (!advancedSettings) {
+      clearErrors(["username", "password"]);
+      // Reset form values if settings are disabled
+      setValue("username", "");
+      setValue("password", "");
+      // Also update the Redux state
+      dispatch(setUsername(""));
+      dispatch(setPassword(""));
+    }
+  }, [advancedSettings]);
+
+  const handleImport = () => {
+    const dpPayload: catalog.CatalogServiceImportApiArg = {
+      url: deploymentPackage.helmChartURL,
+      projectName: SharedStorage.project?.name ?? "",
+    };
+    if (deploymentPackage.username && deploymentPackage.password) {
+      dpPayload.username = deploymentPackage.username;
+      dpPayload.authToken = deploymentPackage.password;
+    }
+
+    importDeploymentPackage(dpPayload);
+    navigate("../packages");
+  };
 
   return (
     <form {...cy} className={className}>
-      <Flex cols={[12]}>
-        <div style={{ paddingBottom: "0.4rem" }}>
+      <Flex cols={[12, 12]} colsMd={[6, 6]} colsLg={[6, 6]}>
+        <div>
+          <FieldLabel>Helm Chart URL *</FieldLabel>
           <Controller
             name="helmChartURL"
             control={control}
@@ -72,140 +122,134 @@ const DeploymentPackageHelmChartInfoForm = ({
               maxLength: 70,
             }}
             render={({ field, formState }) => (
-              <>
-                <TextField
-                  {...field}
-                  label="Helm Chart URL *"
-                  data-cy="helm-chart-url"
-                  maxLength={70}
-                  onInput={(e) => {
-                    const value = e.currentTarget.value;
-                    if (value.length) {
-                      dispatch(setHelmChartURL(value));
-                    }
-                  }}
-                  errorMessage={
-                    formState.errors.helmChartURL?.type === "required"
-                      ? "Helm Chart URL is required"
-                      : formState.errors.helmChartURL?.type === "maxLength"
-                        ? "Helm Chart URL can't be more than 70 characters"
-                        : null
-                  }
-                  validationState={
-                    errors.helmChartURL &&
-                    Object.keys(errors.helmChartURL).length > 0
-                      ? "invalid"
-                      : "valid"
-                  }
-                  size={InputSize.Large}
-                />
-                {!errors.helmChartURL && (
-                  <div style={{ minHeight: "1.55rem" }} />
-                )}
-              </>
+              <TextField
+                {...field}
+                data-cy="helm-chart-url"
+                maxLength={70}
+                onInput={(e) => {
+                  const value = e.currentTarget.value;
+                  dispatch(setHelmChartURL(value));
+                }}
+                errorMessage={
+                  formState.errors.helmChartURL?.type === "required"
+                    ? "Helm Chart URL is required"
+                    : formState.errors.helmChartURL?.type === "maxLength"
+                      ? "Helm Chart URL can't be more than 70 characters"
+                      : null
+                }
+                validationState={
+                  errors.helmChartURL &&
+                  Object.keys(errors.helmChartURL).length > 0
+                    ? "invalid"
+                    : "valid"
+                }
+                size={InputSize.Large}
+              />
             )}
           />
         </div>
       </Flex>
-      <ToggleSwitch
-        data-cy="includeAuth"
-        isSelected={includeAuth}
-        onChange={(value) => {
-          toggleAuthSwitch(value);
-        }}
-        className={`${className}__auto-onboard-switch`}
-      >
-        <FieldLabel size={FieldLabelSize.Large}>Include Auth</FieldLabel>
-      </ToggleSwitch>
-      <Flex cols={[6, 6]}>
-        <div className="deployment-package-import-helm-chart-info-form__field-group--left">
-          <Controller
-            name="username"
-            control={control}
-            rules={{
-              required: false,
-              maxLength: 30,
-            }}
-            render={({ field, formState }) => (
-              <>
-                <TextField
-                  {...field}
-                  data-cy="username"
-                  label="Username"
-                  maxLength={30}
-                  isDisabled={!includeAuth}
-                  onInput={(e) => {
-                    const value = e.currentTarget.value ?? "";
-                    if (value.length) {
-                      dispatch(setUsername(value));
-                    }
-                  }}
-                  errorMessage={
-                    formState.errors.username?.type === "maxLength"
-                      ? "Username can't be more than 30 characters"
-                      : null
-                  }
-                  validationState={
-                    errors.username && Object.keys(errors.username).length > 0
-                      ? "invalid"
-                      : "valid"
-                  }
-                  size={InputSize.Large}
-                />
-                {!errors.username && <div style={{ minHeight: "1.3rem" }} />}
-              </>
-            )}
-          />
-        </div>
 
-        <div className="deployment-package-import-helm-chart-info-form__field-group--right">
-          <Controller
-            name="password"
-            control={control}
-            rules={{
-              required: false,
-              maxLength: 30,
-            }}
-            render={({ field, formState }) => (
-              <>
-                <TextField
-                  {...field}
-                  type="password"
-                  data-cy="password"
-                  maxLength={30}
-                  label="Password"
-                  isDisabled={!includeAuth}
-                  onInput={(e) => {
-                    const value = e.currentTarget.value ?? "";
-                    if (value.length) {
-                      dispatch(setPassword(value));
-                    }
-                  }}
-                  errorMessage={
-                    formState.errors.password?.type === "maxLength"
-                      ? "Password can't be more than 30 characters"
-                      : null
-                  }
-                  validationState={
-                    errors.password && Object.keys(errors.password).length > 0
-                      ? "invalid"
-                      : "valid"
-                  }
-                  size={InputSize.Large}
-                />
-                {!errors.password && <div style={{ minHeight: "1.3rem" }} />}
-              </>
-            )}
-          />
-        </div>
-      </Flex>
-      <Textarea
-        dataCy="description"
-        label="Description"
-        value={description ?? ""}
-        onChange={(e) => dispatch(setDescription(e.currentTarget.value))}
+      <Heading semanticLevel={6}>Advanced Settings</Heading>
+      <AdvancedSettingsToggle
+        message="Allow users to override selected profile values at deployment time?"
+        value={advancedSettings}
+        onChange={(value) => {
+          setAdvancedSettings(value);
+        }}
       />
-      <div className="deployment-package-import-helm-chart-info-form__footer">
+
+      {advancedSettings ? (
+        <Flex
+          cols={[12, 12]}
+          colsMd={[6, 6]}
+          colsLg={[6, 6]}
+          className="dp-auth-fields-container"
+        >
+          <div className="dp-auth-field">
+            <FieldLabel>Username</FieldLabel>
+            <Controller
+              name="username"
+              control={control}
+              rules={{
+                required: false,
+                maxLength: 30,
+              }}
+              render={({ field }) => (
+                <>
+                  <TextField
+                    {...field}
+                    data-cy="username"
+                    maxLength={30}
+                    onInput={(e) => {
+                      const value = e.currentTarget.value ?? "";
+                      dispatch(setUsername(value));
+                    }}
+                    errorMessage={
+                      errors.username?.type === "maxLength"
+                        ? "Username can't be more than 30 characters"
+                        : validationErrors.username // Use local validation error state
+                          ? validationErrors.username
+                          : null
+                    }
+                    validationState={
+                      (errors.username &&
+                        Object.keys(errors.username).length > 0) ||
+                      validationErrors.username
+                        ? "invalid"
+                        : "valid"
+                    }
+                    size={InputSize.Large}
+                  />
+                </>
+              )}
+            />
+          </div>
+
+          <div className="dp-auth-field">
+            <FieldLabel>Password</FieldLabel>
+            <Controller
+              name="password"
+              control={control}
+              rules={{
+                required: false,
+                maxLength: 30,
+              }}
+              render={({ field }) => (
+                <>
+                  <TextField
+                    {...field}
+                    type="password"
+                    data-cy="password"
+                    maxLength={30}
+                    onInput={(e) => {
+                      const value = e.currentTarget.value ?? "";
+                      dispatch(setPassword(value));
+                    }}
+                    errorMessage={
+                      errors.password?.type === "maxLength"
+                        ? "Password can't be more than 30 characters"
+                        : validationErrors.password // Use local validation error state
+                          ? validationErrors.password
+                          : null
+                    }
+                    validationState={
+                      (errors.password &&
+                        Object.keys(errors.password).length > 0) ||
+                      validationErrors.password
+                        ? "invalid"
+                        : "valid"
+                    }
+                    size={InputSize.Large}
+                  />
+                </>
+              )}
+            />
+          </div>
+        </Flex>
+      ) : null}
+
+      <div className="dp-helmchart-form__footer">
         <Button
           onPress={() => {
             navigate("../packages");
@@ -220,6 +264,11 @@ const DeploymentPackageHelmChartInfoForm = ({
           data-cy="importButton"
           size={ButtonSize.Large}
           onPress={() => handleImport()}
+          isDisabled={
+            Object.keys(errors).length > 0 ||
+            Boolean(validationErrors.username) ||
+            Boolean(validationErrors.password)
+          }
         >
           Import
         </Button>
--- apps/app-orch/src/components/pages/DeploymentPackageImportFromHelmChart/DeploymentPackageImportFromHelmChart.tsx
+++ apps/app-orch/src/components/pages/DeploymentPackageImportFromHelmChart/DeploymentPackageImportFromHelmChart.tsx
@@ -2,45 +2,23 @@
  * SPDX-FileCopyrightText: (C) 2023 Intel Corporation
  * SPDX-License-Identifier: Apache-2.0
  */
-import { catalog } from "@orch-ui/apis";
 import { setActiveNavItem, setBreadcrumb } from "@orch-ui/components";
-import { SharedStorage } from "@orch-ui/utils";
 import { Heading } from "@spark-design/react";
-import { useEffect, useMemo, useState } from "react";
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
-import DeploymentPackageHelmChartInfoForm, {
-  PackageInputs,
-} from "src/components/organisms/deploymentPackages/DeploymentPackageHelmChartInfoForm/DeploymentPackageHelmChartInfoForm";
+import { useEffect, useMemo } from "react";
+import DeploymentPackageHelmChartInfoForm from "src/components/organisms/deploymentPackages/DeploymentPackageHelmChartInfoForm/DeploymentPackageHelmChartInfoForm";
 import {
   deploymentPackageBreadcrumb,
   homeBreadcrumb,
   importDeploymentPackageFromHelmChartBreadcrumb,
   packagesNavItem,
 } from "../../../routes/const";
-import { useAppDispatch, useAppSelector } from "../../../store/hooks";
-import { selectDeploymentPackage } from "../../../store/reducers/deploymentPackage";
+import { useAppDispatch } from "../../../store/hooks";
 
 const dataCy = "deploymentPackageImportFromHelmChart";
 
 const DeploymentPackageImportFromHelmChart = () => {
   const cy = { "data-cy": dataCy };
-  const [includeAuth, setIncludeAuth] = useState<boolean>(false);
   const dispatch = useAppDispatch();
-  const deploymentPackage = useAppSelector(selectDeploymentPackage);
-  const navigate = useNavigate();
-  const {
-    control,
-    formState: { errors },
-  } = useForm<PackageInputs>({
-    mode: "all",
-    defaultValues: {
-      helmChartURL: deploymentPackage.helmChartURL,
-      username: deploymentPackage.username,
-      password: deploymentPackage.password,
-    },
-  });
-  const [importDeploymentPackage] = catalog.useCatalogServiceImportMutation();
 
   const breadcrumb = useMemo(
     () => [
@@ -56,20 +34,6 @@ const DeploymentPackageImportFromHelmChart = () => {
     dispatch(setActiveNavItem(packagesNavItem));
   }, [dispatch, breadcrumb]);
 
-  const handleImport = () => {
-    const dpPayload: catalog.CatalogServiceImportApiArg = {
-      url: deploymentPackage.helmChartURL,
-      projectName: SharedStorage.project?.name ?? "",
-    };
-    if (includeAuth) {
-      // auth toggle
-      dpPayload.username = deploymentPackage.username;
-      dpPayload.authToken = deploymentPackage.password;
-    }
-    importDeploymentPackage(dpPayload);
-    navigate("../packages");
-  };
-
   return (
     <div {...cy} className="deployment-package-import-from-helm-chart">
       <Heading semanticLevel={1} size="l" data-cy="title">
@@ -84,13 +48,7 @@ const DeploymentPackageImportFromHelmChart = () => {
         Provide Helm Chart details to import a deployment
       </Heading>
 
-      <DeploymentPackageHelmChartInfoForm
-        control={control}
-        errors={errors}
-        handleImport={handleImport}
-        toggleAuthSwitch={(value) => setIncludeAuth(value)}
-        includeAuth={includeAuth}
-      />
+      <DeploymentPackageHelmChartInfoForm />
     </div>
   );
 };
