e223df4c99e718b742395347bf47707cebab953c
Dariusz Lemiech <dariusz.lemiech@intel.com>
2025-07-02 00:07:44 +0200

    filtering templates based on os profile

--- library/utils/mocks/infra/store/osresources.ts
+++ library/utils/mocks/infra/store/osresources.ts
@@ -15,6 +15,7 @@ export const createOsResource = (
   profileName: string,
   securityFeature: infra.InstanceResourceRead["securityFeature"],
   osType: infra.OperatingSystemResourceRead["osType"],
+  metadata: string,
 ): infra.OperatingSystemResourceRead => {
   return {
     resourceId: id,
@@ -29,6 +30,7 @@ export const createOsResource = (
       '{"Repo":[{"Name":"libpcre2-32-0","Version":"10.42-3","Architecture":"x86_64","Distribution":"tmv3","URL":"https://www.pcre.org/","License":"BSD","Modified":"No"},{"Name":"libpcre2-16-0","Version":"10.42-3","Architecture":"x86_64","Distribution":"tmv3","URL":"https://www.pcre.org/","License":"BSD","Modified":"No"}]}',
     existingCves:
       '[{"cve_id":"CVE-2016-5180","priority":"critical","affected_packages":["fluent-bit-3.1.9-11.emt3.x86_64"]},{"cve_id":"CVE-2021-3672","priority":"medium","affected_packages":["fluent-bit-3.1.9-11.emt3.x86_64"]},{"cve_id":"CVE-2020-8277","priority":"high","affected_packages":["fluent-bit-3.1.9-11.emt3.x86_64"]},{"cve_id":"CVE-2022-4904","priority":"high","affected_packages":["fluent-bit-3.1.9-11.emt3.x86_64"]}]',
+    metadata,
   };
 };
 
@@ -40,6 +42,7 @@ export const osTb = createOsResource(
   "TbOS",
   "SECURITY_FEATURE_SECURE_BOOT_AND_FULL_DISK_ENCRYPTION",
   "OS_TYPE_IMMUTABLE",
+  '{"kubernetesVersion":"v1.28.0"}',
 );
 
 export const osTbUpdate = createOsResource(
@@ -50,6 +53,7 @@ export const osTbUpdate = createOsResource(
   "TbOS",
   "SECURITY_FEATURE_SECURE_BOOT_AND_FULL_DISK_ENCRYPTION",
   "OS_TYPE_IMMUTABLE",
+  '{"kubernetesVersion":"v1.28.0"}',
 );
 
 export const osUbuntu = createOsResource(
@@ -60,6 +64,7 @@ export const osUbuntu = createOsResource(
   "Ubuntu-x86_profile",
   "SECURITY_FEATURE_SECURE_BOOT_AND_FULL_DISK_ENCRYPTION",
   "OS_TYPE_IMMUTABLE",
+  '{"kubernetesVersion":"v1.30.0"}',
 );
 
 export const osRedHat = createOsResource(
@@ -70,6 +75,7 @@ export const osRedHat = createOsResource(
   "Redhat-x86_profile",
   "SECURITY_FEATURE_NONE",
   "OS_TYPE_IMMUTABLE",
+  '{"kubernetesVersion":"v1.32.0"}',
 );
 
 export class OsResourceStore extends BaseStore<
