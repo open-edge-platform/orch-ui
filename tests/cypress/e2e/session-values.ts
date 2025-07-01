35b5510632425b4b1bf6cd8e635ce724f766559c
Chandrasekharan, Vignesh <vignesh.chandrasekharan@intel.com>
2025-07-01 17:10:09 +0530

    license

--- tests/cypress/e2e/session-values.ts
+++ tests/cypress/e2e/session-values.ts
@@ -1,2 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: (C) 2023 Intel Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 export const sessionKey = "<OIDC_SESSION_KEY_FROM_SESSION_STORAGE>";
 export const sessionValue = "<OIDC_VALUE_FROM_SESSION_STORAGE>";
