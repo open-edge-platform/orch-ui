282b1fd9784b6791581012f21110199c008333ae
riyaz-ghati <108117001+riyaz-ghati@users.noreply.github.com>
2025-07-04 14:00:54 +0530

    Deployment package UI updates (#196)
    Co-authored-by: KaliszWiktoria <109806562+KaliszWiktoria@users.noreply.github.com>
    Co-authored-by: Kalisz <wiktoria.kalisz@intel.com>

---
+++ apps/app-orch/src/components/pages/DeploymentPackageImportFromHelmChart/DeploymentPackageImportFromHelmChart.pom.ts
@@ -0,0 +1,47 @@
+/*
+ * SPDX-FileCopyrightText: (C) 2023 Intel Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { AdvancedSettingsTogglePom } from "@orch-ui/components";
+import { CyPom } from "@orch-ui/tests";
+const dataCySelectors = ["helm-chart-url", "username", "password"] as const;
+type Selectors = (typeof dataCySelectors)[number];
+
+class DeploymentPackageImportFromHelmChartPom extends CyPom<Selectors> {
+  public advSettingsPom: AdvancedSettingsTogglePom;
+  constructor(public rootCy = "deploymentPackageGeneralInfoForm") {
+    super(rootCy, [...dataCySelectors]);
+    this.advSettingsPom = new AdvancedSettingsTogglePom();
+  }
+
+  get root() {
+    return cy.get("[data-cy='deploymentPackageImportFromHelmChart']");
+  }
+
+  get title() {
+    return cy.get("[data-cy='title']");
+  }
+
+  get subTitle() {
+    return cy.get("[data-cy='subTitle']");
+  }
+
+  get helmChartUrl() {
+    return cy.get("input[name='helmChartURL']");
+  }
+
+  get username() {
+    return cy.get("input[name='username']");
+  }
+
+  get password() {
+    return cy.get("input[name='password']");
+  }
+
+  getErrors() {
+    return cy.get(".spark-form-field__error-message");
+  }
+}
+
+export default DeploymentPackageImportFromHelmChartPom;
