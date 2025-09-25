52e14d97f502017a3c4b5acfc383fccf4427494c
Chandrasekharan, Vignesh <vignesh.chandrasekharan@intel.com>
2025-09-25 17:40:16 +0530

    mdb updates

--- library/utils/mocks/metadata-broker/msw.ts
+++ library/utils/mocks/metadata-broker/msw.ts
@@ -4,25 +4,31 @@
  */
 
 import { mbApi } from "@orch-ui/apis";
-import { http , delay} from "msw"
+import { delay, http, HttpResponse } from "msw";
 import MetadataStore from "./metadata";
 
 const baseURL = "/resource.orchui-u.apis";
 const metadataStore = new MetadataStore();
 export const handlers = [
-  http.post(`${baseURL}/v1/projects/**/metadata`, async (req, res, ctx) {
-  await delay(500); 
-    const apiResult = await req.json<any, mbApi.MetadataList>();
+  http.post(`${baseURL}/v1/projects/**/metadata`, async ({ request }) => {
+    await delay(500);
+    const apiResult = (await request.json()) as mbApi.MetadataList;
     metadataStore.post(apiResult);
 
-    return HttpResponse.text(
-{status: 200,
-});
+    return HttpResponse.json<mbApi.MetadataServiceCreateOrUpdateMetadataApiResponse>(
+      {
+        metadata: metadataStore.list(),
+      },
+      { status: 200 },
+    );
   }),
-  http.get(`${baseURL}/v1/projects/**/metadata`, async (_, res, ctx) {
-  await delay(500); 
-    return HttpResponse.text(
-{status: 200,
-});
+  http.get(`${baseURL}/v1/projects/**/metadata`, async () => {
+    await delay(500);
+    return HttpResponse.json<mbApi.MetadataServiceGetMetadataApiResponse>(
+      {
+        metadata: metadataStore.list(),
+      },
+      { status: 200 },
+    );
   }),
 ];
