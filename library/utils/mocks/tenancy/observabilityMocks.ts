e81cf7323f8172246d090966e88507e28b3875aa
Chandrasekharan, Vignesh <vignesh.chandrasekharan@intel.com>
2025-09-25 10:49:12 +0530

    fix o11y

--- library/utils/mocks/tenancy/observabilityMocks.ts
+++ library/utils/mocks/tenancy/observabilityMocks.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { http , HttpResponse} from "msw"
+import { http, HttpResponse } from "msw";
 import AlertStore from "../../mocks_new/admin/store/alerts";
 import { SharedStorage } from "../../shared-storage/shared-storage";
 import AlertDefinitionStore from "./data/alertDefinitions";
@@ -22,53 +22,33 @@ const projectName = SharedStorage.project?.name;
 // alerts
 export const handlers = [
   http.get(`${baseURL}/projects/${projectName}/alerts`, () => {
-    return HttpResponse.json(
-{ alerts: as.list() },
-{status: 200,
-});
+    return HttpResponse.json({ alerts: as.list() }, { status: 200 });
+  }),
+  http.get(`${baseURL}/projects/${projectName}/alerts/definitions`, () => {
+    return HttpResponse.json({ alertDefinitions: ads.list() }, { status: 200 });
+  }),
+  http.get(`${baseURL}/projects/${projectName}/alerts/receivers`, () => {
+    return HttpResponse.json({ receivers: rs.list() }, { status: 200 });
   }),
   http.get(
-    `${baseURL}/projects/${projectName}/alerts/definitions`,
-    () => {
-      return HttpResponse.json(
-{ alertDefinitions: ads.list() },
-{status: 200,
-});
-    },
-  ),
-  http.get(
-    `${baseURL}/projects/${projectName}/alerts/receivers`,
-    () => {
-      return HttpResponse.json(
-{ receivers: rs.list() },
-{status: 200,
-});
-    },
-  ),
-  http.get(
     `${baseURL}/projects/${projectName}/alerts/definitions/:alertDefinitionId/template`,
-    () => {
+    ({ params }) => {
       const { alertDefinitionId } = params;
-      return HttpResponse.json(
-adts.get(alertDefinitionId as string),
-{status: 200,
-});
+      return HttpResponse.json(adts.get(alertDefinitionId as string), {
+        status: 200,
+      });
     },
   ),
   http.patch(
     `${baseURL}/projects/${projectName}/alerts/receivers/:receiverId`,
     () => {
-      return HttpResponse.text(
-{status: 204,
-});
+      return HttpResponse.json(null, { status: 204 });
     },
   ),
   http.patch(
     `${baseURL}/projects/${projectName}/alerts/definitions/:alertDefinitionId`,
     () => {
-      return HttpResponse.text(
-{status: 503,
-});
+      return HttpResponse.json(null, { status: 503 });
     },
   ),
 ];
