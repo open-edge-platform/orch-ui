3e18db2bd100649a33d595e08f4ffd3d7ab9466c
riyaz C Ghati <riyaz.chandasaheb.ghati@intel.com>
2025-10-01 10:39:46 +0530

    Added delete update policy flow

--- apps/infra/src/components/pages/OsUpdatePolicy/OsUpdatePolicy.tsx
+++ apps/infra/src/components/pages/OsUpdatePolicy/OsUpdatePolicy.tsx
@@ -6,14 +6,17 @@
 import { infra } from "@orch-ui/apis";
 import {
   ApiError,
+  MessageBannerAlertState,
   Popup,
   Table,
   TableColumn,
   TableLoader,
 } from "@orch-ui/components";
-import { SharedStorage } from "@orch-ui/utils";
+import { parseError, SharedStorage } from "@orch-ui/utils";
 import { Heading, Icon, Text } from "@spark-design/react";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setMessageBanner } from "../../../store/notifications";
 import OsUpdatePolicyDetailsDrawer from "./OsUpdatePolicyDrawer";
 
 import "./OsUpdatePolicy.scss";
@@ -22,7 +25,7 @@ interface OsUpdatePolicyProps {}
 const OsUpdatePolicy = ({}: OsUpdatePolicyProps) => {
   const cy = { "data-cy": dataCy };
   const className = "o-s-profiles";
-
+  const dispatch = useDispatch();
   const [showDrawer, setShowDrawer] = useState<boolean>(false);
   const [selectedOsUpdatePolicy, setSelectedOsUpdatePolicy] = useState<
     infra.OsUpdatePolicyRead | undefined
@@ -37,6 +40,37 @@ const OsUpdatePolicy = ({}: OsUpdatePolicyProps) => {
     projectName: SharedStorage.project?.name ?? "",
     pageSize: 100,
   });
+
+  const [deleteOsUpdatePolicy] =
+    infra.useOsUpdatePolicyDeleteOsUpdatePolicyMutation();
+
+  const deleteOsUpdatePolicyHandler = (osUpdatePolicyId) => {
+    deleteOsUpdatePolicy({
+      projectName: SharedStorage.project?.name ?? "",
+      resourceId: osUpdatePolicyId,
+    })
+      .unwrap()
+      .then(() => {
+        dispatch(
+          setMessageBanner({
+            icon: "check-circle",
+            text: "OS Update Policy has been deleted.",
+            title: "Deletion Succeeded",
+            variant: MessageBannerAlertState.Success,
+          }),
+        );
+      })
+      .catch((error) => {
+        dispatch(
+          setMessageBanner({
+            icon: "alert-triangle",
+            text: parseError(error).data,
+            title: "Deletion Failed",
+            variant: MessageBannerAlertState.Error,
+          }),
+        );
+      });
+  };
 
   const getContent = () => {
     if (isLoading) {
@@ -79,7 +113,7 @@ const OsUpdatePolicy = ({}: OsUpdatePolicyProps) => {
       Header: "Action",
       textAlign: "center",
       padding: "0",
-      accessor: (item) => {
+      accessor: (item: infra.OsUpdatePolicyRead) => {
         return (
           <Popup
             dataCy="osProfilesPopup"
@@ -90,6 +124,12 @@ const OsUpdatePolicy = ({}: OsUpdatePolicyProps) => {
                 onSelect: () => {
                   setShowDrawer(true);
                   setSelectedOsUpdatePolicy(item);
+                },
+              },
+              {
+                displayText: "Delete",
+                onSelect: () => {
+                  deleteOsUpdatePolicyHandler(item?.resourceId);
                 },
               },
             ]}
