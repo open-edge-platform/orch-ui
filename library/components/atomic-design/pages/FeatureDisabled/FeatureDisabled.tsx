38bf013b499dea0709a0aabff18dd00c030b10e3
Chandrasekharan, Vignesh <vignesh.chandrasekharan@intel.com>
2026-02-06 14:08:28 +0530

    make a FeatureDisabled component to handle disabled routes

--- library/components/atomic-design/pages/FeatureDisabled/FeatureDisabled.tsx
+++ library/components/atomic-design/pages/FeatureDisabled/FeatureDisabled.tsx
@@ -1,12 +1,11 @@
 /*
- * SPDX-FileCopyrightText: (C) 2023 Intel Corporation
+ * SPDX-FileCopyrightText: (C) 2026 Intel Corporation
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Button, Heading } from "@spark-design/react";
+import { Button, Heading, Icon } from "@spark-design/react";
 import { ButtonSize } from "@spark-design/tokens";
-import { useCallback } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./FeatureDisabled.scss";
 
 export interface FeatureDisabledProps {
@@ -17,31 +16,21 @@ export const FeatureDisabled = ({
   featureName = "This feature",
 }: FeatureDisabledProps) => {
   const navigate = useNavigate();
-  const location = useLocation();
 
-  const getRootPath = useCallback(() => {
-    const parts = location.pathname.split("/");
-
-    // all paths containing more than two path segments means its MFE
-    // (application under container), e.g. /app-orch/...
-    // then they navigate to its own root path
-    return parts.length > 2 ? `/${parts[1]}` : "/";
-  }, [location]);
-
-  const to = getRootPath();
+  const to = "/dashboard";
   return (
     <div className="feature-disabled" data-cy="featureDisabled">
       <div className="feature-disabled__container">
-        <Heading semanticLevel={1}>Service Disabled</Heading>
-        <p>{featureName} is currently disabled.</p>
+        <Icon icon="gears" className="feature-disabled__icon" />
+        <Heading semanticLevel={5}>Service Disabled</Heading>
+        <p>{featureName} is currently disabled in your orchestrator.</p>
 
         <Button
           data-cy="home"
-          style={{ marginLeft: "auto" }}
           onPress={() => navigate(to)}
           size={ButtonSize.Large}
         >
-          Home
+          Go to Dashboard
         </Button>
       </div>
     </div>
